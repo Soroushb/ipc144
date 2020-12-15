f943363e54a0ac95d76a72da333077a9fb7fc5cf
Soroush <61368137+Soroushb@users.noreply.github.com>
2020-12-15 15:11:09 -0500

    Add files via upload

---
+++ assignment1/ms2/contacts.h
@@ -0,0 +1,39 @@
+//==============================================
+// Name:           Soroush Bahrami
+// Student Number: 152499182
+// Email:          sbahrami7@myseneca.ca
+// Section:        NHH 144
+// Date:           
+//==============================================
+// Assignment:     1
+// Milestone:      1
+//==============================================
+
+// Structure type Name declaration
+struct Name 
+{
+    char firstName[31];
+    char middleInitial[7];
+    char lastName[36];
+};
+
+// Structure type Address declaration
+struct Address
+{
+    int streetNumber;
+    char street[41];
+    int apartmentNumber;
+    char postalCode[8];
+    char city[41];
+};
+    // Place your code here...
+
+
+// Structure type Numbers declaration
+struct Numbers
+{
+    char cell[11];
+    char home[11];
+    char business[11];
+};// Place your code here...
+
